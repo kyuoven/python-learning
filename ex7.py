75a4381c67530d6a1b6bba38093d77ddbbc42b7d
Lucas <kyuoven@duck.com>
2021-12-17 16:00:27 +0100

    ex7.py: add exercise seven

---
+++ ex7.py
@@ -0,0 +1,27 @@
+# print command
+print("Mary had a litle lamb.")
+# ??? ask about format() command -> asked! check ex7_k.py file!!!!!
+print("Its fleece was white as {}.".format('snow'))
+# print command
+print("And everywhere that Mary went.")
+# print command plus dot times 10 -> prinst
+print("." * 10) # what'd that do?
+
+# assigning variables for lines 11 til 22
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch the comma at the end . try removing it to see what happens
+# putting variables in a string
+print(end1 + end2 + end3 + end4 + end5 + end6, end='') 
+print(end7 + end8 + end9 + end10 + end11 + end12 )
