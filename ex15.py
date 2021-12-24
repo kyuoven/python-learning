115888b093a6be3883e725e45fa3ffd6c086b69e
Lucas <kyuoven@duck.com>
2021-12-24 15:36:03 +0100

    ex15.py: Add exercise fifteen

---
+++ ex15.py
@@ -0,0 +1,17 @@
+# from the module 'sys' import the arguement variable 'argv'
+from sys import argv
+# listing and assigning arguement variables
+script, filename = argv
+# assigning a function to a variable = to do something when typed
+txt =  open(filename)
+# regular printing
+print(f"Here's your file {filename} :")
+# reading the text file
+print(txt.read())
+# print command and input
+print("Type the filename again:")
+file_again = input(">")
+# open file again after having input filename
+txt_again = open(file_again)
+# print sample text again
+print(txt_again.read())
