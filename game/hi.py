0b279268c9ae53b2d1dc4d67051d03767ac53800
Lucas <kyuoven@duck.com>
2022-01-13 09:52:53 +0100

    hi.py: early stages game

---
+++ game/hi.py
@@ -0,0 +1,48 @@
+print("Hi.")
+print("Press 'enter' to continue.")
+
+input("> ")
+
+print("Welcome to Me!")
+print("Press 'enter' to continue.")
+
+input("> ")
+
+print(f"What is your name?")
+
+your_name = input("> ")
+
+print(f"Lovely! What a nice name you have, {your_name}. :-)")
+print(f"How was your day today, {your_name}?")
+
+day = input("> ")
+
+if day == "Good" "Great" "Amazing" "Splendind" "Wonderful" "OK" "Okay" "Well" "Went well" "Fantastic":
+    print("Nice to hear!")
+    print("Do you want to reflect on anything that happened today?")
+    print("1. Yes")
+    print("2. No")
+
+yes_or_no = input("> ")
+
+if yes_or_no == 1:
+    print("Tell me all about your day, then! :-) <3!")
+
+elif yes_or_no == 2:
+    print("Okay! I will respect your boundaries.")
+
+if day == "Meh" "Bad" "Not good" "Horrible" "Like shit" "Like crap" "Shitty" "Poor" "Unpleasant" "Awful" "Depressing" "Distressing" "Terrible" :
+    print("I hope you feel better soon!")
+    print("Would you like to talk to me about your day?")
+    print("1. Yes")
+    print("2. No")
+
+yes_or_no = input("> ")
+
+if yes_or_no == 1:
+    print("Tell me all about your day, then! :-) <3!")
+
+elif yes_or_no == 2:
+    print("Okay! I will respect your boundaries.")
+
+
