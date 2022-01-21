67d53433ec87cd7188c0644736bb994586f4b3e3
Lucas <kyuoven@duck.com>
2022-01-21 10:01:20 +0100

    ex40_cumzone.py: funny hehe

---
+++ ex40_cumzone.py
@@ -0,0 +1,30 @@
+class Song(object):
+    def __init__(self, lyrics):
+        self.lyrics = lyrics
+
+    def sing_for_me(self):
+        for line in self.lyrics:
+            print(line)
+
+
+WTTCZ = Song(
+    [
+        "Welcome to the CUM ZONE",
+        "Only cum inside anime girls",
+        "Quivering clit, double jointed pussy",
+        "Fresh balls, elegant ejaculation",
+        "First the kiss, then the cum",
+        "My dick is in love with pain",
+        "Co-op cock torture",
+        "Stuff my dick into a furnace",
+        "Stitch my cock shut",
+        "Pressure cook my greasy balls",
+        "Cumblast me and make it snappy",
+        "Cum, cum, cum, cum, cum, cum, cum, cum",
+        "Cum, cum, cum, cum, cum, cum, cum, cum",
+        "Cum, cum, cum, cum, cum, cum, cum, cum",
+        "Cum, cum, cum, cum, cum, cum, cum, cum",
+    ]
+)
+
+WTTCZ.sing_for_me()
