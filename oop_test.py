cab94497603c5bd1d3a1bcc77a610d54a18b1b4a
lucas <yoshisthickass@outlook.com>
2022-01-23 23:15:38 +0100

    oop_test.py: confusing

--- oop_test.py
+++ oop_test.py
@@ -2,7 +2,7 @@ import random
 from urllib.request import urlopen
 import sys
 
-WORD_URL = "http:/learncodethehardway.org/words.txt"
+WORD_URL = "http://learncodethehardway.org/words.txt"
 WORDS = []
 
 PHRASES = {
@@ -26,30 +26,27 @@ for word in urlopen(WORD_URL).readlines():
 
 
 def convert(snippet, phrase):
-    class_names = [w.capitalize() for w in
-                    random.sample(WORDS, snippet.count("%%%"))
-    other_names = random.sample(WORDS, snippet.count("***"))]
+    class_names = [w.capitalize() for w in random.sample(WORDS, snippet.count("%%%"))]
+    other_names = random.sample(WORDS, snippet.count("***"))
     results = []
     param_names = []
 
-    for i in range(0, snippet.count("@@@")) :
-        param_count = random.randint(1,3)
-        param_names.append(','.join(
-            random.sample(WORDS,param_count)))
+    for i in range(0, snippet.count("@@@")):
+        param_count = random.randint(1, 3)
+        param_names.append(",".join(random.sample(WORDS, param_count)))
 
     for sentence in snippet, phrase:
         result = sentence[:]
 
-        #fake class names
+        # fake class names
         for word in class_names:
-            result = result.replace("%%%", word,1)
+            result = result.replace("%%%", word, 1)
 
-
-        #fake other names
+        # fake other names
         for word in other_names:
             result = result.replace("***", word, 1)
 
-        #fake parameter lists
+        # fake parameter lists
         for word in param_names:
             result = result.replace("@@@", word, 1)
 
@@ -57,7 +54,8 @@ def convert(snippet, phrase):
 
     return results
 
-#keep going until they hit CTRL-D
+
+# keep going until they hit CTRL-D
 try:
     while True:
         snippets = list(PHRASES.keys())
@@ -69,7 +67,6 @@ try:
             if PHRASE_FIRST:
                 question, answer = answer, question
 
-            
             print(question)
 
             input("> ")
