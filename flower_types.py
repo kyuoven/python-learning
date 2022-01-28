40c16c8c2c4c7fa53a4c696f678fb82e390295e0
Lucas <kyuoven@duck.com>
2022-01-28 20:28:51 +0100

    flower_types.py: flower types

---
+++ flower_types.py
@@ -0,0 +1,19 @@
+FLOWER_TYPES = [
+    "Cherry_Blossom ",
+    "Dahlia ",
+    "Sunflower",
+    "Tulips",
+    "Orchid",
+    "Aloe_Vera",
+    "Red_Aglaonema",
+    "African_Mask_Plant",
+    "Bonzai",
+    "Lavender",
+    "Oxalis",
+    "Rex_Begonia",
+    "String_of_Pearls",
+    "Peace_Lily",
+    "African_Violet",
+]
+
+print(FLOWER_TYPES)
