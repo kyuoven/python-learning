33600852f8c6483f069fcefa42bcaac42f806a4e
Lucas <kyuoven@duck.com>
2022-04-04 14:05:44 +0200

    ex51_How_The_Web_Works: Finished writing theory

--- ex51_How_The_Web_Works.py
+++ ex51_How_The_Web_Works.py
@@ -49,8 +49,23 @@
 # and receiving bytes over the network wires between your pc and others at the url.
 
 # Request: Your browser is connected using the address you gave. Now it needs to ask for
-# the resource it wants on the remote server
+# the resource it wants on the remote server. If you gave /book/ at the end of the URL,
+# then you want the file (resource) at /book/, and most servers will use the real file
+# /book/index.html but pretend  it doesn't exist. What the browser actually does to get this
+# is send a request to the server. It has to send something to query the server for this request.
+# The interesting thing is that these "resources" don't have to be files. When the browser in your
+# application asks for something, the server is returning something your Python code
+# generated.
 
-# Server:
+# Server: The server is the computer at the end of a browser's connection that knows how to
+# answer your browser's requests for files/sources. MOst web servers just send files, and that's
+# actually the majority of traffic. But you're actually building a server in Python that knows how
+# to take requests for resources and then return strings that you craft using Python. When you do this
+# crafting, you are pretending to be a file to the browser, but really it's just code. As you can see,
+# from exercise 50, it also doesn't take much code to create a response
 
-# Response:
+# Response: Thisis the HTML (CSS, JavaScript, or images) your server wants to send back to the browser
+# as the answer to the browser's request. In the case of files, it just reads them off the disk and
+# sends them back to the browser, but it wraps the contents of the disk in a special "header"
+# so the browser knows what it's getting. In the case of your application, you're still sending the same
+# thing, including the header, but you generate the data on the fly with you Python code.
