ec8f42f73d3994111771656a810ce3345a5b1107
Lucas <kyuoven@duck.com>
2022-06-07 14:36:37 +0200

    39.01: dict practice

--- 39.01_Dictionaries_Oh_Lovely_Dictionaries.py
+++ 39.01_Dictionaries_Oh_Lovely_Dictionaries.py
@@ -1,3 +1,24 @@
-foods = {"rice", "pudding", "soup", "wraps", "pulled pork", "noodles"}
-drinks = {"cola", "fanta", "coffee"}
-#
+# 0-10 of how much i like it
+foods_dict = {
+    "rice": 8,
+    "pudding": 6,
+    "soup": 5,
+    "wraps": 7,
+    "pulled pork": 9.5,
+    "noodles": 7,
+    "salame": 6,
+    "cheese": 7,
+    "bread": 8,
+}
+
+# same thing here
+drinks_dict = {
+    "cola": 9,
+    "fanta": 8,
+    "coffee": 10,
+    "water": 5,
+    "protein shake": 10,
+}
+
+# "Instead of only being able to use positions, you can use almost anything to access the elements within a dictionary.
+# This lets you treat a dict like it's a database for storing and organizing data."
